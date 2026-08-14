99c2f95e23d7069217b63b80f8b7134853f4998e
gdsjayjang <gdsjayjang@gmail.com>
2026-08-15 00:39:53 +0900

    dataset

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,9 +1,19 @@
+import io
+import gdown
 import numpy as np
 import pandas as pd
 import streamlit as st
 
 def data_loader():
-    path = './data/'
-    data = 'data_clean.parquet'
+    FILE_ID = '1D6qTqLyiTczXc6q4xwZuzWHgvmgOB04B'
+    url = f'https://drive.google.com/uc?id={FILE_ID}'
+    # path = './data/'
+    # data = 'data_clean.parquet'
+
+    # 2. 메모리 버퍼로 다운로드
+    output = io.BytesIO()
+    gdown.download(url, output, quiet=True)
+    output.seek(0)
     
-    return pd.read_parquet(path + data)
+    df = pd.read_parquet(output)
+    return df
